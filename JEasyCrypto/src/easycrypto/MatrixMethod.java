ef9f05b0f20edd8e351649bf091728a3caae014f
jennitormala <tormalajenni@gmail.com>
2019-11-20 18:40:53 +0200

    handle string as Unicode code points instead of chars in Matrix encryption method

--- JEasyCrypto/src/easycrypto/MatrixMethod.java
+++ JEasyCrypto/src/easycrypto/MatrixMethod.java
@@ -5,63 +5,64 @@ import easycrypto.EasyCryptoAPI.ResultCode;
 
 class MatrixMethod implements CryptoMethod {
 
-	@Override
-	public Result encrypt(final String toEncrypt) {
-		String toStoreTo = new String();
-		int matrixWidth = (int) Math.floor(Math.sqrt(toEncrypt.length()));
+    @Override
+    public Result encrypt(final String toEncrypt) {
+        String toStoreTo = new String();
+        int toEncryptLength = toEncrypt.codePointCount(0, toEncrypt.length());
+        int matrixWidth = (int) Math.floor(Math.sqrt(toEncryptLength));
+        String toRotate;
+        String tmp = "";
+        int extraCount = (int) Math.abs(Math.pow(matrixWidth, 2) - toEncryptLength);
+        if (extraCount > 0) {
+            tmp = toEncrypt.substring(0, toEncrypt.offsetByCodePoints(0, extraCount));
+            tmp = new StringBuilder(tmp).reverse().toString();
+            toRotate = toEncrypt.substring(toEncrypt.offsetByCodePoints(0, extraCount));
+        } else {
+            toRotate = toEncrypt;
+        }
+        for (int outer = 0; outer < matrixWidth; outer++) {
+            toStoreTo += toRotate.substring(toRotate.offsetByCodePoints(0, outer), toRotate.offsetByCodePoints(0, outer + 1));
+            int toRotateLength = toRotate.codePointCount(0, toRotate.length());
+            for (int inner = outer + matrixWidth; inner < toRotateLength; inner += matrixWidth) {
+                toStoreTo += toRotate.substring(toRotate.offsetByCodePoints(0, inner), toRotate.offsetByCodePoints(0, inner + 1));
+            }
+        }
+        if (tmp.length() > 0) {
+            toStoreTo += tmp;
+        }
+        return new Result(ResultCode.ESuccess, toStoreTo);
+    }
 
-		String toRotate;
-		String tmp = "";
-		int extraCount = (int) Math.abs(Math.pow(matrixWidth, 2) - toEncrypt.length());
-		if (extraCount > 0) {
-			tmp = toEncrypt.substring(0,extraCount);
-			tmp = new StringBuilder(tmp).reverse().toString();
-			toRotate = toEncrypt.substring(extraCount);
-		} else {
-			toRotate = toEncrypt;
-		}
-		for (int outer = 0; outer < matrixWidth; outer++) {
-			toStoreTo += toRotate.substring(outer,outer+1);
-			for (int inner = outer+matrixWidth; inner < toRotate.length(); inner += matrixWidth) {
-				toStoreTo += toRotate.substring(inner,inner+1);
-			}
-		}
-		if (tmp.length() > 0) {
-			toStoreTo += tmp;
-		}
-		return new Result(ResultCode.ESuccess, toStoreTo);
-	}
+    @Override
+    public Result decrypt(final String toDecrypt) {
+        String toStoreTo = new String();
+        int matrixWidth = (int) Math.floor(Math.sqrt(toDecrypt.length()));
 
-	@Override
-	public Result decrypt(final String toDecrypt) {
-		String toStoreTo = new String();
-		int matrixWidth = (int)Math.floor(Math.sqrt(toDecrypt.length()));
+        String toRotate;
+        String tmp = "";
+        int extraCount = (int) Math.abs(Math.pow(matrixWidth, 2) - toDecrypt.length());
+        if (extraCount > 0) {
+            tmp = toDecrypt.substring(toDecrypt.length() - extraCount, (toDecrypt.length() - extraCount) + extraCount);
+            tmp = new StringBuilder(tmp).reverse().toString();
+            toRotate = toDecrypt.substring(0, toDecrypt.length() - extraCount);
+        } else {
+            toRotate = toDecrypt;
+        }
+        for (int outer = 0; outer < matrixWidth; outer++) {
+            toStoreTo += toRotate.substring(outer, outer + 1);
+            for (int inner = outer + matrixWidth; inner < toRotate.length(); inner += matrixWidth) {
+                toStoreTo += toRotate.substring(inner, inner + 1);
+            }
+        }
+        if (tmp.length() > 0) {
+            toStoreTo = tmp + toStoreTo;
+        }
+        return new Result(ResultCode.ESuccess, toStoreTo);
+    }
 
-		String toRotate;
-		String tmp = "";
-		int extraCount = (int)Math.abs(Math.pow(matrixWidth, 2) - toDecrypt.length());
-		if (extraCount > 0) {
-			tmp = toDecrypt.substring(toDecrypt.length()-extraCount,(toDecrypt.length()-extraCount)+extraCount);
-			tmp = new StringBuilder(tmp).reverse().toString();
-			toRotate = toDecrypt.substring(0, toDecrypt.length()-extraCount);
-		} else {
-			toRotate = toDecrypt;
-		}
-		for (int outer = 0; outer < matrixWidth; outer++) {
-			toStoreTo += toRotate.substring(outer,outer+1);
-			for (int inner = outer+matrixWidth; inner < toRotate.length(); inner += matrixWidth) {
-				toStoreTo += toRotate.substring(inner,inner+1);
-			}
-		}
-		if (tmp.length() > 0) {
-			toStoreTo = tmp + toStoreTo;
-		}
-		return new Result(ResultCode.ESuccess, toStoreTo);
-	}
-
-	@Override
-	public String method() {
-		return "matrix";
-	}
+    @Override
+    public String method() {
+        return "matrix";
+    }
 
 }
